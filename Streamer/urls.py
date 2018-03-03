50a7e425ebd0f61db96afb4a8284f14160952881
stark03 <tusharmurarka@gmail.com>
2018-03-03 17:49:28 +0530

    slight changes

--- Streamer/urls.py
+++ Streamer/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
 	
 	url(r'^tweets/$', views.TweetsView.as_view(), name = 'tweets'),
 	url(r'^users/$', views.UsersView.as_view(), name = 'users'),
-	path('keyword/<str:keyword>/', views.StreamTweetsWithKeyword),
+	path('keyword/<str:keyword>/<int:time_limit>/', views.StreamTweetsWithKeyword),
+	path('csv/',views.exportAsCSV),
 
 ]
 
--- Streamer/views.py
+++ Streamer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.filters import SearchFilter,OrderingFilter
 from django.urls import reverse
 from django.http import HttpResponse
 from .serializers import TweetSerializer, UserSerializer
@@ -13,6 +13,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework import generics
 from django.utils import timezone
 import time
+import csv
 from .pagination import TweetLimitOffsetPagination, UserLimitOffsetPagination
 
 
@@ -39,18 +40,30 @@ class UsersView(generics.ListAPIView):
 	queryset = User.objects.all()
 
 
-
-
-
-def StreamTweetsWithKeyword(request, keyword):
+def StreamTweetsWithKeyword(request, keyword, time_limit):
 	
-	s = StreamListener(10, keyword)
+	s = StreamListener(time_limit, keyword)
 	stream = tweepy.Stream(auth = api.auth, listener = s)
 	stream.filter(track = [keyword])	
-	#time.sleep(4)
-	#stream.stop()
-	return redirect(reverse('tweet'))
+	return redirect(reverse('tweets'))
 
 
+def exportAsCSV(request):
+	response = HttpResponse(content_type='text/csv')
+	response['Content-Disposition'] = 'attachment; filename="tweets.csv"'
+	
+	"""Handpicked some importanat fields"""
 
+	imp_fields = ['created_at', 'text', 'retweet_count', 'favorite_count']
+	writer = csv.writer(response)
+	writer.writerow(imp_fields)
+	q = Tweet.objects.all()
+	for x in q:
+		writer.writerow([
+			x.created_at,
+			x.text,
+			x.retweet_count,
+			x.favorite_count,	
+		])
 
+	return response
